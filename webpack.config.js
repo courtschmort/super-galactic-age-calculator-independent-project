b61495f17f8eab92f0781adc317fed9d1efa91a2
Courtney Schild <courtneyschild@gmail.com>
2019-11-15 08:12:02 -0800

    update cloned starter-template repository from GitHub and install npm.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Starter Template',
+      title: 'Super Galactic Age Calculator Independent Project',
       template: './src/index.html',
       inject: 'body'
     })

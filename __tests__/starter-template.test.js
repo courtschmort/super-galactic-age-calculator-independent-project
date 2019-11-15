93462edd7434be9f51c476f9f66413f3d5d322a8
Courtney Schild <courtneyschild@gmail.com>
2019-11-15 10:20:57 -0800

    write a coded test for returning a person's age in Earth days; test should fail.

--- __tests__/starter-template.test.js
+++ __tests__/starter-template.test.js
@@ -1,1 +1,8 @@
-// import
+import { User } from "./../src/super-galactic-age-calculator.js";
+
+describe("User", () => {
+  test("should return person's age in Earth days", () => {
+    let days = new User(30, 78);
+    expect(days.calcYearsToDays()).toEqual(10950);
+  });
+})
--- src/super-galactic-age-calculator.js
+++ src/super-galactic-age-calculator.js
@@ -5,7 +5,7 @@ export class User {
   }
 
   calcYearsToDays() {
-
+    
   }
 
   calcMercury() {

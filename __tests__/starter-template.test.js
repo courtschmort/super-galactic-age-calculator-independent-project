0f10018bdc5517853055356fff530f865814662b
Courtney Schild <courtneyschild@gmail.com>
2019-11-15 10:31:01 -0800

    update variable names to be more specific.

--- __tests__/starter-template.test.js
+++ __tests__/starter-template.test.js
@@ -2,7 +2,8 @@ import { User } from "./../src/super-galactic-age-calculator.js";
 
 describe("User", () => {
   test("should return person's age in Earth days", () => {
-    let days = new User(30, 78);
-    expect(days.calcYearsToDays()).toEqual(10950);
+    let daysEarth = new User(30, 78);
+    expect(daysEarth.calcYearsToDays()).toEqual(10950);
   });
+  
 })

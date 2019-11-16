a027f620cd896189ad7634f8a81ac9d2c938b882
Courtney Schild <courtneyschild@gmail.com>
2019-11-15 16:21:50 -0800

    add form and style page with CSS.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-// import
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+
+  });
+});
